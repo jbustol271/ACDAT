fff29652cef4eefa0d54c477ebb5c4f71262aae2
Javier Bustamante <jbustol271@g.educaand.es>
2023-10-15 10:51:05 +0200

    1 Version Ejercicio.4.4.1

---
+++ src/com/acdat/unit2/MiCopyFile.java
@@ -0,0 +1,10 @@
+package com.acdat.unit2;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class MiCopyFile {
+    private FileInputStream fileInputStream = null;
+    private FileOutputStream fileOutputStream = null;
+    private byte[] datos = new byte[128];
+}
